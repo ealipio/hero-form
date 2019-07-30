b887396ecc470a6466e6f72c6a3435257eabf676
Eisson Alipio <eisson.alipio@gmail.com>
2019-07-30 15:01:33 -0500

    feat: adding  img to counter component

--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -5,19 +5,26 @@ export interface CounterProps {
 }
 
 export interface CounterState {
-    counter: number
+    counter: number,
+    imageUrl: string,
 }
 
 class Counter extends React.Component<CounterProps, CounterState> {
     constructor(props: CounterProps) {
         super(props);
-        this.state = { counter: 10 };
+        this.state = { counter: 0, imageUrl: 'https://picsum.photos/150' };
     }
     render() {
         return <React.Fragment>
-            <span> {this.state.counter} </span>
+            <img src={this.state.imageUrl} style={{borderRadius: '50%'}} />
+            <span> {this.formatCounter()} </span>
             <button className="btn btn-secondary">Increment</button>
         </React.Fragment>;
+    }
+
+    formatCounter() {
+        const {counter} = this.state;
+        return counter === 0 ? <h2>Zero</h2> : counter;
     }
 }
 
