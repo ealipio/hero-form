ec61ee58af87e4b091861c0364a764bbe18866c3
Eisson Alipio <eisson.alipio@gmail.com>
2019-07-30 14:08:36 -0500

    feat: using React.Fragment

--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -11,7 +11,10 @@ export interface CounterState {
  
 class Counter extends React.Component<CounterProps, CounterState> {
     render() { 
-        return ( <h1>Hello World!</h1> );
+        return <React.Fragment>
+            <h3>Hello World</h3>
+            <button>Increment</button>
+        </React.Fragment> ;
     }
 }
  
