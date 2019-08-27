598eae823e1ce7a344d0f8bd6b2d1d5a9c7bca85
Eisson Alipio <eisson.alipio@gmail.com>
2019-08-26 23:22:01 -0500

    feat: showing how middlewares works

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const express = require("express");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
+// how middleware works
+app.use((req, res, next) => {
+  console.log(Date.now());
+  req.name = "Eisson Alipio";
+  next();
+});
 //index route
 app.get("/", (req, res) => {
+  console.log(req.name);
   res.send("Hello Peru!");
 });
 
