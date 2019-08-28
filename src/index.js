c9b83735d07d4eb2792460626ee896c5404b3300
Eisson Alipio <eisson.alipio@gmail.com>
2019-08-28 14:47:43 -0500

    feat: using body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+const connectionURL = process.env.CONNECTION_URL;
 app.set("port", process.env.PORT || 3000);
+// --------------------------
+mongoose.connect(
+  connectionURL,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected");
+    }
+  }
+);
+
+// require('./models/Idea');
+
+// const Idea = mongoose.model('ideas');
+// --------------------------
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // how middleware works
 app.use((req, res, next) => {
@@ -11,6 +36,7 @@ app.use((req, res, next) => {
   req.name = "Eisson Alipio";
   next();
 });
+
 //index route
 app.get("/", (req, res) => {
   console.log(req.name);
@@ -19,6 +45,15 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("About this corporation");
+});
+
+app.get("/ideas/add", (req, res) => {
+  res.send("add Ideas");
+});
+
+app.post("/ideas", (req, res) => {
+  console.log(req.body);
+  res.send({ message: "You are right, she is the best one" });
 });
 
 app.listen(app.get("port"), () => {
